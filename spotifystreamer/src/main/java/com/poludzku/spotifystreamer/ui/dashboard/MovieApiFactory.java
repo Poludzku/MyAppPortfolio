0117ad1e4b8b3df48d4fd4884af8d47a52c9b05a
Jacek Kulinski <Jacek@Jaceks-MBP.home>
2016-11-23 12:52:43 +0000

    update retrofit

--- spotifystreamer/src/main/java/com/poludzku/spotifystreamer/ui/dashboard/MovieApiFactory.java
+++ spotifystreamer/src/main/java/com/poludzku/spotifystreamer/ui/dashboard/MovieApiFactory.java
@@ -1,18 +1,28 @@
 package com.poludzku.spotifystreamer.ui.dashboard;
 
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by greed on 25/10/15.
  */
 public class MovieApiFactory {
     public MovieApi getMovieApi() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
+
+
         return new Retrofit.Builder().baseUrl("http://api.themoviedb.org")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build()
+
                 .create(MovieApi.class);
 
     }
